26eca2e176fce12e8b91420e781154e42309d877
Ahmed Alshatebi <ahmedalshatibi22@gmail.com>
2025-07-18 18:58:25 +0300

    pagination pages connecting

--- app/components/PaginationBar.tsx
+++ app/components/PaginationBar.tsx
@@ -11,6 +11,7 @@ export default function PaginationBar({ currentId, totalPages = 6, basePath }: P
   const id = Number(currentId);
 
   return (
+
     <div className="w-full flex justify-center items-center py-6 bg-gray-900">
       <nav className="flex items-center gap-2">
         <Link
